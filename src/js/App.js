5eca940ff3ec0eeca5ba0002daa506cba47f745d
ORKUMA, Michael Msuega <orkuma.michael70@gmail.com>
2023-07-02 09:51:24 +0100

    Work Gallery

--- src/js/App.js
+++ src/js/App.js
@@ -3,7 +3,6 @@
 
 let btnSend = document.querySelector('.nav-toggler');
 let navOpen = document.querySelector('.navbar-nav')
-
 btnSend.addEventListener('click', (e) => {
     navOpen.classList.toggle('navbar-open');
 })
