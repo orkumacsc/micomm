66ea2a8fb6f9ba414a2253f8d291a5f422870fde
ORKUMA, Michael Msuega <orkuma.michael70@gmail.com>
2023-08-12 17:10:40 +0100

    Form Submission

--- src/js/App.js
+++ src/js/App.js
@@ -12,3 +12,39 @@ btnOpen.addEventListener('click', () => {
 btnClose.addEventListener('click', () => {
     menu.classList.add('none');
 })
+
+
+// Form Controller
+const msgForm = document.getElementById("sendMessage");
+const msg = document.getElementById('msg');
+const url = "https://formspree.io/f/xbjvgadn";
+
+const handleSubmit = async (e) => {
+    e.preventDefault();    
+    const formData = new FormData(e.target);
+    fetch(url, {method: msgForm.method, body: formData, headers: {'Accept': 'Application/json'}})
+    .then(response => {
+        if(response.ok){
+            msg.innerHTML = 'Thanks for contacting me. I\'ll get in touch with you ASAP.';
+            msg.style.padding = "1rem";
+            msg.style.color = "#00cc8f";
+            msgForm.reset();
+        } else{
+            response.json().then(data => {
+                if(Object.hasOwn(data, 'errors')){
+                    msg.innerHTML = data['errors'].map(error => error['message']).json(', ');
+                    msg.style.padding = "1rem";
+                    msg.style.color = "red";
+                }else{
+                    msg.innerHTML = 'Ooops! There was a problem submitting your form.'
+                }
+            })
+        }
+    }).catch(error => {
+        msg.innerHTML = 'Ooops! There was a problem submitting your form.';
+        msg.style.padding = "1rem";
+        msg.style.color = "red";
+    })    
+}
+
+msgForm.addEventListener("submit", handleSubmit);
