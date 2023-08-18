fa150794956ee6b5dcf213cc23f4f2f01ca3a799
ORKUMA, Michael Msuega <orkuma.michael70@gmail.com>
2023-08-18 08:09:10 +0100

    Resume Modal

--- src/js/App.js
+++ src/js/App.js
@@ -48,3 +48,25 @@ const handleSubmit = async (e) => {
 }
 
 msgForm.addEventListener("submit", handleSubmit);
+
+// resumeModal
+
+const btn = document.getElementById('open-modal');
+const closeBtn = document.getElementById('close');
+const modal = document.getElementById('resumeModal');
+const body = document.getElementById('body');
+
+btn.addEventListener('click', () => {
+    modal.style.display = 'block';
+    body.style.overflow = 'hidden';
+})
+closeBtn.addEventListener('click', () => {
+    modal.style.display = 'none';
+    body.style.overflow = 'auto';
+})
+window.addEventListener('click', (e) => {
+    if(e.target == modal){
+    modal.style.display = 'none';
+    body.style.overflow = 'auto';
+    }
+})
