4c058d33dc7e1e77268b66993797ed7a75001faf
ORKUMA, Michael Msuega <orkuma.michael70@gmail.com>
2023-08-19 21:10:53 +0100

    Optimised

--- src/js/App.js
+++ src/js/App.js
@@ -3,15 +3,24 @@
 
 const btnOpen = document.querySelector('.toggle-open');
 const btnClose = document.querySelector('.toggle-close');
-let menu = document.querySelector('.navbar');
+let menu = document.getElementById('navbar');
+let navLink = document.querySelectorAll('.nav-link');
 
-btnOpen.addEventListener('click', () => {
+btnOpen.addEventListener('click', () => {    
     menu.classList.remove('none');
+    menu.style.animation = 'slide-in ease 600ms forwards';
+    menu.style.right = '-100%';
 })
 
-btnClose.addEventListener('click', () => {
-    menu.classList.add('none');
+btnClose.addEventListener('click', () => { 
+    menu.style.animation = 'slide-out ease 600ms forwards';
+    menu.style.right = '0';
 })
+
+navLink.forEach(n => n.addEventListener('click', () => {
+    menu.style.animation = 'slide-out ease 600ms forwards';
+    menu.style.right = '0';
+}))
 
 
 // Form Controller
@@ -49,8 +58,8 @@ const handleSubmit = async (e) => {
 
 msgForm.addEventListener("submit", handleSubmit);
 
-// resumeModal
 
+// resumeModal
 const btn = document.getElementById('open-modal');
 const closeBtn = document.getElementById('close');
 const modal = document.getElementById('resumeModal');
@@ -58,15 +67,22 @@ const body = document.getElementById('body');
 
 btn.addEventListener('click', () => {
     modal.style.display = 'block';
+    modal.style.animation = 'slide-down ease 600ms forwards';
+    modal.style.top = '-100%';
     body.style.overflow = 'hidden';
+    
 })
-closeBtn.addEventListener('click', () => {
-    modal.style.display = 'none';
-    body.style.overflow = 'auto';
+closeBtn.addEventListener('click', () => {    
+    modal.style.animation = 'slide-up ease 600ms forwards';
+    modal.style.top = '0';
+    body.style.overflow = 'auto';    
 })
+
+// Close modal when clicked outside modal content
 window.addEventListener('click', (e) => {
     if(e.target == modal){
-    modal.style.display = 'none';
+    modal.style.animation = 'slide-up ease 600ms forwards';
+    modal.style.top = '0';
     body.style.overflow = 'auto';
     }
 })
